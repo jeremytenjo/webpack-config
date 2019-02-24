1c44571b10e183051693fe2249f02badb86ecd0f
Jeremy <tenjojeremy@gmail.com>
2019-02-24 12:46:47 -0800

    .

--- loaders/loaders.index.js
+++ loaders/loaders.index.js
@@ -2,5 +2,6 @@ const js_jsx = require('./loader.js.jsx.js')
 const css = require('./loader.css')
 const fonts = require('./loader.fonts')
 const images = require('./loader.images')
+const md = require('./loader.md')
 
-module.exports = [js_jsx, css, fonts, images]
+module.exports = [js_jsx, css, md, fonts, images]
