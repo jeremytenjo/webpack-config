3d6d9676a9274913baad7c02afb2e75d9a43cbb2
Jeremy <tenjojeremy@gmail.com>
2019-02-26 20:08:32 -0800

    .

--- configOptions/plugins/compression-webpack-plugin.js
+++ configOptions/plugins/compression-webpack-plugin.js
@@ -3,5 +3,4 @@ const compression_webpack_plugin = require('compression-webpack-plugin')
 module.exports = () =>
   new compression_webpack_plugin({
     algorithm: 'gzip',
-    deleteOriginalAssets: true,
   })
