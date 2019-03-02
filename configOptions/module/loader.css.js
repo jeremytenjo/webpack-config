166d37f0bacc6f38f528497722b55fdbe28832b6
Jeremy <tenjojeremy@gmail.com>
2019-03-01 18:20:10 -0800

    .

--- configOptions/module/loader.css.js
+++ configOptions/module/loader.css.js
@@ -1,11 +1,7 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const devMode = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   test: /\.css$/,
-  use: [
-    {
-      loader: MiniCssExtractPlugin.loader,
-    },
-    'css-loader',
-  ],
+  use: [devMode ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
 }
