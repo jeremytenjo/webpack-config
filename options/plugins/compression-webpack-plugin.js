2d29ab024685d4790e8064edf4b27d9ac4373f73
Jeremy <tenjojeremy@gmail.com>
2019-03-24 16:52:12 -0700

    upddated to brolti plugin

--- options/plugins/compression-webpack-plugin.js
+++ options/plugins/compression-webpack-plugin.js
@@ -1,6 +1,7 @@
-const compression_webpack_plugin = require('compression-webpack-plugin')
+// const compression_webpack_plugin = require('compression-webpack-plugin')
+const compression_webpack_plugin = require('brotli-webpack-plugin')
 
 module.exports = () =>
   new compression_webpack_plugin({
-    algorithm: 'gzip',
+    asset: '[path].br[query]',
   })
