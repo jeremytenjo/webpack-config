fdf7547e827c264bc4516f4ee9fcd19c9c806b54
Jeremy <tenjojeremy@gmail.com>
2019-03-24 16:48:09 -0700

    return to gsip conpression

--- options/plugins/compression-webpack-plugin.js
+++ options/plugins/compression-webpack-plugin.js
@@ -2,7 +2,5 @@ const compression_webpack_plugin = require('compression-webpack-plugin')
 
 module.exports = () =>
   new compression_webpack_plugin({
-    algorithm: 'brotliCompress',
-    filename: '[path].br[query]',
-    compressionOptions: { level: 11 },
+    algorithm: 'gzip',
   })
