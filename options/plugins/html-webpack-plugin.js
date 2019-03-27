aafadb7419918a4c9a469b4018271dfb77e3e9d3
Jeremy <tenjojeremy@gmail.com>
2019-03-26 20:16:32 -0700

    v3.42.0

--- options/plugins/html-webpack-plugin.js
+++ options/plugins/html-webpack-plugin.js
@@ -26,13 +26,13 @@ module.exports = (webpackManifest) => {
     appMountId: 'root',
     bodyHtmlSnippet: `${bodyHtmlSnippet}<noscript>You need to enable JavaScript to run this app.</noscript>`,
     headHtmlSnippet: `
+    <base href="/"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=0">
 		<meta name="description" content="${description}">
 		<meta name="apple-mobile-web-app-title" content="${name}">
 		<meta name="apple-mobile-web-app-status-bar-style" content="${primary}">
 		<meta name="apple-mobile-web-app-capable" content="yes">
 		<meta name="msapplication-tap-highlight" content="no">
-     <base href="/"/>
      <link rel="apple-touch-icon" href="images/public/icon_152x152.png">
      <link rel="apple-touch-icon" sizes="152x152" href="images/public/icon_152x152.png">
      <link rel="apple-touch-icon" sizes="180x180" href="images/public/icon_180x180.png">
