d32fff8936342bce9a8f199b4fad5777c6612d98
Jeremy Tenjo <jeremy.tenjo.fcvinteractive.com>
2019-03-28 11:33:07 -0700

    Added woff font

--- tests/entry.js
+++ tests/entry.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './body.css'
 import './span.css'
-import './Roboto-Regular.ttf'
-
+import './fonts/roboto.woff2'
+import './fonts/montserrat.woff2'
 import Comp from './testComponent/comp.index'
 ReactDOM.render(<Comp />, document.getElementById('root'))
--- tests/testComponent/comp.index.js
+++ tests/testComponent/comp.index.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Comp = () => (
   <div>
+    <h1>Working</h1>
+    <h2>Working</h2>
     <span>Working!</span>
   </div>
 )
