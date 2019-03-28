fbf5770a3fc35d753b19c145fb8291bdf4397bda
Jeremy <tenjojeremy@gmail.com>
2019-03-28 07:37:46 -0700

    added fonts to test

--- tests/entry.js
+++ tests/entry.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './body.css'
 import './span.css'
+import './Roboto-Regular.ttf'
 
 import Comp from './testComponent/comp.index'
 ReactDOM.render(<Comp />, document.getElementById('root'))
