56d6eb13a30e8ec2c693d3af45e817a221918085
Jeremy <tenjojeremy@gmail.com>
2019-06-24 21:11:38 -0700

    updated to tenjojermey

--- bin/webpack-scripts.js
+++ bin/webpack-scripts.js
@@ -4,13 +4,13 @@ const shell = require('shelljs')
 
 // Project
 const action = process.argv[2]
-const configFilePath = `${process.cwd()}/node_modules/@tenjo/webpack-config/configs/config.project.js`
+const configFilePath = `${process.cwd()}/node_modules/@tenjojeremy/webpack-config/configs/config.project.js`
 const startCommand = `webpack-dev-server --config ${configFilePath} --mode development --hot --inline`
 const buildCommand = `webpack --config ${configFilePath} -p`
 const analyzeCommand = `webpack-bundle-analyzer --port 4200 ${process.cwd()}/build/stats.json`
 
 // Library
-const ligBconfigFilePath = `${process.cwd()}/node_modules/@tenjo/webpack-config/configs/config.library.js`
+const ligBconfigFilePath = `${process.cwd()}/node_modules/@tenjojeremy/webpack-config/configs/config.library.js`
 const libBuildCommand = `webpack --config ${ligBconfigFilePath} -p`
 
 switch (action) {
