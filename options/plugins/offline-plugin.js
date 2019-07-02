ce6ac3926ef56765495a4b1e5604eb6877e17e07
Jeremy <tenjojeremy@gmail.com>
2019-07-02 13:06:50 -0700

    updaed offline plugin for SPA pages

--- options/plugins/offline-plugin.js
+++ options/plugins/offline-plugin.js
@@ -3,5 +3,11 @@ var OfflinePlugin = require('offline-plugin')
 module.exports = (mode) => {
   const isProd = mode === 'production'
 
-  return isProd ? new OfflinePlugin() : () => null
+  return isProd
+    ? new OfflinePlugin({
+        publicPath: '/',
+        appShell: '/',
+        externals: ['/'],
+      })
+    : () => null
 }
