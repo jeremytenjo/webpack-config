e95bce2bda1058428959702adba7c79ac54ef044
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-11-30 15:03:22 -0700

    manifest: create manifest in dev

--- options/plugins/webpack-pwa-manifest.js
+++ options/plugins/webpack-pwa-manifest.js
@@ -2,9 +2,7 @@ const dirPath = require('path')
 
 const WebpackPwaManifest = require('webpack-pwa-manifest')
 
-module.exports = (webpackManifest, mode) => {
-  const isProd = mode === 'production'
-
+module.exports = (webpackManifest) => {
   const {
     projectInfo: {
       name = 'Project Name',
@@ -16,26 +14,24 @@ module.exports = (webpackManifest, mode) => {
     },
   } = webpackManifest
 
-  return isProd
-    ? new WebpackPwaManifest({
-        fingerprints: false,
-        name,
-        short_name: shortName,
-        description,
-        background_color: background,
-        theme_color: primary,
-        orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
-        display: 'standalone',
-        share_target,
-        icons: [
-          {
-            src: dirPath.resolve(path),
-            sizes: [92, 152, 180, 167, 192, 512],
-            destination: dirPath.join('images', 'public'),
-          },
-        ],
-      })
-    : () => null
+  return new WebpackPwaManifest({
+    fingerprints: false,
+    name,
+    short_name: shortName,
+    description,
+    background_color: background,
+    theme_color: primary,
+    orientation: 'portrait',
+    scope: '/',
+    start_url: '/',
+    display: 'standalone',
+    share_target,
+    icons: [
+      {
+        src: dirPath.resolve(path),
+        sizes: [92, 152, 180, 167, 192, 512],
+        destination: dirPath.join('images', 'public'),
+      },
+    ],
+  })
 }
