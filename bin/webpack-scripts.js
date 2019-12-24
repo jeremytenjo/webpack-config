601d6769b2a12b41881d1ea69dab05674a720730
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2019-12-24 11:53:03 -0700

    webpack : added color to build output

--- bin/webpack-scripts.js
+++ bin/webpack-scripts.js
@@ -9,7 +9,7 @@ const action = process.argv[2]
 const manifestPath = process.argv[3] || `${process.cwd()}/.webpack.manifest.js`
 const configFilePath = wappRoot('bin/configs/config.project.js')
 const startCommand = `webpack-dev-server --config ${configFilePath} --mode development --color --hot --inline --manifestPath ${manifestPath}`
-const buildCommand = `webpack --config ${configFilePath} -p --manifestPath ${manifestPath}`
+const buildCommand = `webpack --config ${configFilePath} -p --manifestPath ${manifestPath} --color`
 const analyzeCommand = `webpack-bundle-analyzer --port 4200 ${process.cwd()}/build/stats.json`
 
 switch (action) {
