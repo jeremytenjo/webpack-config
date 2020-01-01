45be3961f4665d2c3b14df218ab60413e04df792
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-01-01 17:44:40 -0500

    version bump

--- examples/react/registerServiceWorker.js
+++ examples/react/registerServiceWorker.js
@@ -1,5 +1,12 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-  })
+  navigator.serviceWorker.register('/service-worker.js').then(
+    function(registration) {
+      console.log('Service worker registration succeeded:', registration)
+    },
+    function(error) {
+      console.log('Service worker registration failed:', error)
+    },
+  )
+} else {
+  console.log('Service workers are not supported.')
 }
