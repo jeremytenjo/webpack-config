ce6ab9dac38021da8af54377f38cc0f10adaaef1
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-01-01 17:31:01 -0500

    registerServiceWorker

--- examples/react/index.js
+++ examples/react/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-require('offline-plugin/runtime').install()
 
 import './body.css'
 import './span.css'
 import './fonts/roboto.woff2'
 import './fonts/montserrat.woff2'
+import './registerServiceWorker'
 import Comp from './testComponent/comp.index'
 ReactDOM.render(<Comp />, document.getElementById('root'))
