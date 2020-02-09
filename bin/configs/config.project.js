9164a84f4cdb2f8595db9b10d3516c337863ad3e
jeremy.tenjo <jeremy.tenjo@fcvinteractive.com>
2020-02-09 15:12:21 -0500

    shortened

--- bin/configs/config.project.js
+++ bin/configs/config.project.js
@@ -4,27 +4,27 @@ module.exports = (env, argv) => {
   // Manifest
   const webpackManifest = require(manifestPath)
   // Entry
-  const getEntry = require('../../options/entry/entry.index')
+  const getEntry = require('../../options/entry')
   const entry = getEntry(webpackManifest)
   // Output
-  const getOutput = require('../../options/output/output.index')
+  const getOutput = require('../../options/output')
   const output = getOutput(webpackManifest)
   // Devtool
-  const getDevtool = require('../../options/devtool/devtool.index')
+  const getDevtool = require('../../options/devTool')
   const devtool = getDevtool(webpackManifest)
   // DevServer
-  const getDevServer = require('../../options/devServer/devServer.index')
+  const getDevServer = require('../../options/devServer')
   const devServer = getDevServer(webpackManifest)
   // Performance
-  const getPerformance = require('../../options/performance/performance.index')
+  const getPerformance = require('../../options/performance')
   const performance = getPerformance(webpackManifest)
   // Optimization
-  const getOptimization = require('../../options/optimization/optimization.index')
+  const getOptimization = require('../../options/optimization')
   const optimization = getOptimization(webpackManifest)
   // Module
-  const modules = require('../../options/module/module.index')
+  const modules = require('../../options/module')
   // Plugins
-  const getPlugins = require('../../options/plugins/plugins.index')
+  const getPlugins = require('../../options/plugins')
 
   return {
     entry,
